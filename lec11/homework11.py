67c107ae4d34ace590b2a2f528224b463688be04
yougesh dulal <dulalyougesh74096@gmail.com>
2026-01-02 19:05:19 +0900

    Update homework11.py

--- lec11/homework11.py
+++ lec11/homework11.py
@@ -13,5 +13,8 @@ def transcribe_wavefile(filename, language):
     @returns:
     text (str) - the recognized speech
     '''
-    raise RuntimeError("FAIL!!  You need to change this function so it works!")
-        
+    r = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = r.record(source)
+    text = r.recognize_google(audio, language=language)
+    return text
