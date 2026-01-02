e74e384901a4bbb775e914dfd53151adc324bb90
yougesh dulal <dulalyougesh74096@gmail.com>
2026-01-02 18:28:38 +0900

    Update homework4.py

--- lec04/homework4.py
+++ lec04/homework4.py
@@ -1,4 +1,3 @@
-
 def next_birthday(date, birthdays):
     '''
     Find the next birthday after the given date.
@@ -12,7 +11,21 @@ def next_birthday(date, birthdays):
     birthday - the next day, after given date, on which somebody has a birthday
     list_of_names - list of all people with birthdays on that date
     '''
-    birthday = (1,1)
-    list_of_names = []
-    return birthday, list_of_names
-    
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    def next_day(m, d):
+        if d < month_days[m - 1]:
+            return (m, d + 1)
+        if m < 12:
+            return (m + 1, 1)
+        return (1, 1)
+
+    m, d = date
+    current = next_day(m, d)  
+
+    for _ in range(366):
+        if current in birthdays and birthdays[current]:
+            return current, birthdays[current]
+        current = next_day(*current)
+
+    return (1, 1), []
